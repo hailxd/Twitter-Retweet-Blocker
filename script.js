ef4b868ad5c873ab54f91ad0181bc0cfa7d4c5fa
CJY <jyealc@gmail.com>
2024-07-14 07:41:41 +0800

    Revert "Changed DOMNodeInserted to Mutation Observer"
    This reverts commit 711be9ca1d6f77779fb0f95d57916fd527b45607.

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 // ==UserScript==
 // @name         Hide Retweets
-// @version      1.5
+// @version      1.4
 // @author       Hail
 // @match        https://x.com/*
 // ==/UserScript==
 
 function findArticleAncestor(el) {
-    let current = el;
-    const maxDepth = 6;
-
-    for (let depth = 0; current && depth < maxDepth; depth++) {
-        if (current.tagName === 'ARTICLE') {
-            return current.parentNode?.parentNode || null;
-        }
-        current = current.parentNode;
-    }
-
-    return null;
+    const article = el.closest('article');
+    return article ? article.parentNode.parentNode : null;
 }
 
-function removeRetweets(nodes) {
-    nodes.forEach(node => {
-        if (node.nodeType === 1) {
-            [...node.querySelectorAll("div.r-15zivkp")]
-                .map(findArticleAncestor)
-                .forEach(el => el && el.parentElement.removeChild(el));
-        }
-    });
+function removeRetweets() {
+    [...document.querySelectorAll(".r-15zivkp")]
+		.map(findArticleAncestor)
+		.forEach(el => el && el.parentElement.removeChild(el));
 }
 
 const observer = new MutationObserver(mutations => {
     mutations.forEach(mutation => {
         if (mutation.addedNodes.length) {
-            removeRetweets(mutation.addedNodes);
+            removeRetweets();
         }
     });
 });
