8b7233c3ec2e3fbe2c4fe875f1cc04560d983a42
hailxd <77497878+hailxd@users.noreply.github.com>
2024-07-20 20:58:33 +0800

    Reduced Duplicate checks

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name         Hide Retweets
-// @version      1.9
+// @version      1.10
 // @author       Hail
 // @match        https://x.com/*
+// @grant        GM_addStyle
 // ==/UserScript==
 
 new MutationObserver(m => {
   if (!location.href.includes("/status/") && m.length > 8)
     document.querySelectorAll('.r-15zivkp').forEach(e => {
-      const a = e.closest('div[data-testid="cellInnerDiv"]');
-      if (a) a.style.display = 'none';
+      	const a = e.closest('div[data-testid="cellInnerDiv"]');
+		if (a) {a.style.display = 'none';e.classList.remove('r-15zivkp');}
     });
 }).observe(document.body, { childList: true, subtree: true });
