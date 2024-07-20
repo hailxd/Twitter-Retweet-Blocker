4c67483f4fa785c0c367955cb06fc4040c2d0ffb
hailxd <77497878+hailxd@users.noreply.github.com>
2024-07-20 18:40:34 +0800

    Variables are a waste of code

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 // ==UserScript==
 // @name         Hide Retweets
-// @version      1.8
+// @version      1.9
 // @author       Hail
 // @match        https://x.com/*
 // ==/UserScript==
 
-const observer = new MutationObserver(mutations => {
-  if (!window.location.href.includes("/status/") && mutations.length > 8) {
-    document.querySelectorAll(".r-15zivkp").forEach(el => {
-      const article = el.closest('div[data-testid="cellInnerDiv"]');
-      if (article) article.style.display = 'none';
+new MutationObserver(m => {
+  if (!location.href.includes("/status/") && m.length > 8)
+    document.querySelectorAll('.r-15zivkp').forEach(e => {
+      const a = e.closest('div[data-testid="cellInnerDiv"]');
+      if (a) a.style.display = 'none';
     });
-  }
-});
-
-observer.observe(document.body, { childList: true, subtree: true });
+}).observe(document.body, { childList: true, subtree: true });
